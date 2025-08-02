3d729f2c535c2f50064540916a361fff22995d2d
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-08-02 01:12:00 -0600

    primer commit de omoikane

---
+++ src/omoikane/elearning/OmoikaneElearning.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package omoikane.elearning;
+
+/**
+ *
+ * @author aczay
+ */
+public class OmoikaneElearning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Este es el repo para omoikane");
+    }
+    
+}
