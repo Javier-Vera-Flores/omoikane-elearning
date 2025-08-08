67f3c17fd06374201e3344ecf67dc0a3a5c790d8
wen-ra <78555916+wen-ra@users.noreply.github.com>
2025-08-08 06:22:01 -0600

    Upload
    Actualiza Archivos

--- src/omoikane/elearning/Modelo/Progreso.java
+++ src/omoikane/elearning/Modelo/Progreso.java
@@ -9,41 +9,13 @@ package omoikane.elearning.Modelo;
  * @author aczay
  */
 public class Progreso {
-    // Atributos para el progreso del estudiante
-    private int id_progreso;
-    private double puntaje_promedio;
-    private int id_estudiante;
 
-    public Progreso(int id_progreso, double puntaje_promedio, int id_estudiante) {
-        this.id_progreso = id_progreso;
-        this.puntaje_promedio = puntaje_promedio;
-        this.id_estudiante = id_estudiante;
+    public Progreso(int i, double puntaje, int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    public int getId_progreso() {
-        return id_progreso;
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    public void setId_progreso(int id_progreso) {
-        this.id_progreso = id_progreso;
-    }
-
-
-    public double getPuntaje_promedio() {
-        return puntaje_promedio;
-    }
-
-
-    public void setPuntaje_promedio(double puntaje_promedio) {
-        this.puntaje_promedio = puntaje_promedio;
-    }
-
-    public int getId_estudiante() {
-        return id_estudiante;
-    }
-
-
-    public void setId_estudiante(int id_estudiante) {
-        this.id_estudiante = id_estudiante;
-    }
+    // Atributos y métodos para el progreso del estudiante
 }
